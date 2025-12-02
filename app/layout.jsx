a613fcf60617e1dc023dc2849e74d0993921131c
RSTM12 <140751554+RSTM12@users.noreply.github.com>
2025-12-03 05:58:03 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,34 +7,29 @@ export const metadata = {
   openGraph: {
     title: "DONUT GENESIS",
     description: "Mint Exclusive NFT",
-    // 👇 GANTI MANUAL
-    url: "https://donut-genesis.vercel.app", 
+    url: "https://donut-genesis.vercel.app",
     siteName: "Donut Mint",
-    images: [{ 
-      // 👇 GANTI MANUAL (JANGAN LUPA /donut.jpg DI BELAKANG)
-      url: "https://donut-genesis.vercel.app/donut.jpg" 
+    images: [{
+      url: "https://donut-genesis.vercel.app/donut.jpg"
     }],
   },
   other: {
     "fc:frame": "vNext",
-    // 👇 GANTI MANUAL (JANGAN LUPA /donut.jpg DI BELAKANG)
     "fc:frame:image": "https://donut-genesis.vercel.app/donut.jpg",
     "fc:frame:image:aspect_ratio": "1:1",
-    
-    "fc:frame:button:1": "BUKA APP 🍩",
+    "fc:frame:button:1": "OPEN APP 🍩",
     "fc:frame:button:1:action": "link",
-    // 👇 GANTI MANUAL
     "fc:frame:button:1:target": "https://donut-genesis.vercel.app",
-    
-    // 👇 GANTI MANUAL
-    "fc:frame:post_url": "https//donut-genesis.vercel.app",
+    "fc:frame:post_url": "https://donut-genesis.vercel.app",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white">{children}</body>
+      <body className="bg-white text-black">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
