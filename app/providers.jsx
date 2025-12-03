d03c258245e420350228575cea0c964b7c2466db
RSTM12 <140751554+RSTM12@users.noreply.github.com>
2025-12-03 09:51:19 +0700

    Update providers.jsx

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,10 +1,9 @@
 "use client";
 
-// Kita matikan dulu Wagmi biar HP tidak keberatan/crash
 export function Providers({ children }) {
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    <>
       {children}
-    </div>
+    </>
   );
 }
